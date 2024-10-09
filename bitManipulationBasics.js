55135984f2f8f0ef7bf3ee0dd7078390c029e61b
rishiraj Bhowmik <rishiraj.bhowmik5@gmail.com>
2024-10-10 02:20:51 +0530

    basic properties of bit manipulation added.

---
+++ bitManipulationBasics.js
@@ -0,0 +1,37 @@
+
+// Operators
+//  AND
+// &
+// Bitwise OR
+// |
+// Bitwise XOR
+// ^
+// Bitwise Not
+// ~
+// Left shift operator
+// <<
+// Right shift operator
+// >>
+
+// Properties of Bitwise Operators
+// AND
+// A & 1 = 0 or 1 (depending on the last bit of A)
+// A & 0 =0
+// A & A=A
+
+
+
+// OR
+// A | 0 = A
+// A | 1 = A (if A is odd) 
+//          =A+1(if A is even)
+// A | A = A
+// XOR
+// A ^ 0 = A
+// A ^ A = 0
+
+
+
+let a=5;//101
+let b=6;//110
+console.log(a&b);//100  ->4(Decimal)
